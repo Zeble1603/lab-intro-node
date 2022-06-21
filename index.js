57f3a6e2b097df4dd08650b6f93dd2be97332e36
Blaise <blaisedecarvalho@gmail.com>
2022-06-21 15:55:55 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b){return a - b})
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos<this.items.length && pos >= 0){
+      return this.items[pos]
+    }else{
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length !== 0){
+      let newList= this.items.sort(function(a, b){return b - a})
+      return newList[0]
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length !== 0){
+      let newList= this.items.sort(function(a, b){return a - b})
+      return newList[0]
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      0
+    );
+  }
 
-  avg() {}
+  avg() {
+    if(this.length>0){
+      let sum = this.sum()
+      return sum / this.length
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
